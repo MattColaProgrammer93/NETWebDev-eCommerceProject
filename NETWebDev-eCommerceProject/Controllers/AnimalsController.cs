4b879962b69b718bc10efe5db91b04680e4658ea
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-08-02 18:01:03 -0700

    Create animal fully functional, added some styling to the page.

--- NETWebDev-eCommerceProject/Controllers/AnimalsController.cs
+++ NETWebDev-eCommerceProject/Controllers/AnimalsController.cs
@@ -1,13 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NETWebDev_eCommerceProject.Data;
+using NETWebDev_eCommerceProject.Models;
 
 namespace NETWebDev_eCommerceProject.Controllers
 {
     public class AnimalsController : Controller
     {
+        // Do not reassign this
+        private readonly AnimalCreationContext _context;
+
+        public AnimalsController(AnimalCreationContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Animal a)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add to DB
+                _context.Animals.Add(a); // Prepares insert
+                _context.SaveChanges(); // Executes pending insert
+
+                // Show success message on page
+                ViewData["Message"] = $"{a.Name} was made and added successfully!";
+
+                return View();
+            }
+
+            return View(a);
         }
     }
 }
