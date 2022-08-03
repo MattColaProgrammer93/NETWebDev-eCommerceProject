5de64c1642a43a9b0da29d2432a6804bc2a849be
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-08-02 17:35:42 -0700

    Added a Create Animal view

---
+++ NETWebDev-eCommerceProject/Controllers/AnimalsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NETWebDev_eCommerceProject.Controllers
+{
+    public class AnimalsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
