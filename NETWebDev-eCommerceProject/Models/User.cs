801f4da1c74632d8fa77613b16d076d35625db84
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-08-18 11:53:18 -0700

    Made UserName optional

--- NETWebDev-eCommerceProject/Models/User.cs
+++ NETWebDev-eCommerceProject/Models/User.cs
@@ -5,13 +5,13 @@ namespace NETWebDev_eCommerceProject.Models
     public class User
     {
         [Key]
-        public int MyProperty { get; set; }
+        public int UserId { get; set; }
 
-        public string UserName { get; set; }
+        public string? UserName { get; set; } 
 
-        public string Password { get; set; }
+        public string Password { get; set; } = null!;
 
-        public string Email { get; set; }
+        public string Email { get; set; } = null!;
     }
 
     public class RegisterViewModel
