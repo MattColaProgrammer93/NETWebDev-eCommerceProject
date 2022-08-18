0daa94a039f66dfca16849da586aaa76ae852c22
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-08-18 10:29:37 -0700

    Added the User class

---
+++ NETWebDev-eCommerceProject/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NETWebDev_eCommerceProject.Models
+{
+    public class User
+    {
+        [Key]
+        public int MyProperty { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+    }
+}
