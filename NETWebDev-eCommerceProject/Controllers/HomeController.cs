2be13d31e05b9d10269fbe1cac0afab2085ac8a4
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-08-30 10:36:21 -0700

    Removed Privacy view and links.

--- NETWebDev-eCommerceProject/Controllers/HomeController.cs
+++ NETWebDev-eCommerceProject/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace NETWebDev_eCommerceProject.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
