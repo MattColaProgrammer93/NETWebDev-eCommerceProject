2bc8f63426a78dd77769f969b58572689705e352
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-08-30 09:10:25 -0700

    Added deck button to animal catalog

---
+++ NETWebDev-eCommerceProject/Controllers/DeckController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NETWebDev_eCommerceProject.Data;
+using NETWebDev_eCommerceProject.Models;
+
+namespace NETWebDev_eCommerceProject.Controllers
+{
+    public class DeckController : Controller
+    {
+        // Do not reassign this
+        private readonly AnimalCreationContext _context;
+
+        public DeckController(AnimalCreationContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Add(int id)
+        {
+            Animal? animalToAdd = _context.Animals.Where(a => a.AnimalId == id).SingleOrDefault();
+
+            if (animalToAdd == null)
+            {
+                // Animal with specified id does not exist
+                TempData["Message"] = "Sorry, the animal no longer exists";
+                return RedirectToAction("Index", "Animals");
+            }
+
+            // Todo: Add item to a deck cookie
+            TempData["Message"] = "Animal added to deck";
+            return RedirectToAction("Index", "Animals");
+        }
+    }
+}
